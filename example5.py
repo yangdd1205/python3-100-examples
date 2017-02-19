bc02bf3d8ba3c5263dd62831ed1e23a7c045d9a2
yangdd <yangdd1205@126.com>
2017-02-19 12:59:15 +0800

    20170219

---
+++ example5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*-* coding: UTF-8 -*-
+
+__author__ = 'yangdd'
+
+'''
+ example 005
+'''
+
+
+def num_sort():
+  array = []
+  
+  array.append(int(input("请输入第一个整数：")))
+  array.append(int(input("请输入第二个整数：")))
+  array.append(int(input("请输入第三个整数：")))
+  
+  sorted_array = sorted(array)
+  
+  print(sorted_array)
+  
+  
+  
+if __name__=='__main__':
+  num_sort()
+  
+  
