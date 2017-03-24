5a8fc1a207c0687d4a0870dfe101f7032273ce6d
yangdd <yangdd1205@126.com>
2017-03-24 19:56:26 +0800

    20170324

---
+++ example38.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+	example 038
+'''
+if __name__ == '__main__':
+    a = []
+    sum = 0.0
+    for i in range(3):
+        a.append([])
+        for j in range(3):
+            a[i].append(float(input("input num:\n")))
+    for i in range(3):
+        sum += a[i][i]
+    print(sum)
