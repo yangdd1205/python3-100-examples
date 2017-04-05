bc662e824e9665840d082a4f3d9e4ee26141224e
yangdd <yangdd1205@126.com>
2017-04-05 22:15:12 +0800

    20170405

---
+++ example50.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import random
+
+__author__ = 'yangdd1205'
+
+
+"""
+    随机数
+"""
+
+for i in range(10):
+    print(int(random.random()*10))
+    print(random.randint(10, 20))
