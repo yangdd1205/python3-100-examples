cec038606e2bef5e6f462ca837e6d128528c0647
yangdd <yangdd1205@126.com>
2017-04-15 12:57:03 +0800

    20170415

---
+++ example60.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from tkinter import *
+
+__author__ = "yang.dd"
+
+"""
+    example 060
+"""
+
+if __name__ == '__main__':
+    string = input('please input a string: ')
+    print('%s length is %d' % (string, len(string)))
