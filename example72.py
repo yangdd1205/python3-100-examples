a59c4ee4a27b6cdab90610814bf35938a79b70d8
yangdd <yangdd1205@126.com>
2017-04-27 09:18:25 +0800

    20170427

---
+++ example72.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+__author__ = "yang.dd"
+
+"""
+    example 072
+"""
+
+if __name__ == '__main__':
+    num = []
+    for i in range(3):
+        num.append(int(input("请输入一个数字：")))
+    print(num)
