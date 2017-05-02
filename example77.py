f3c3d3406aa52b1298dab319cb9774a687db22a3
yangdd <yangdd1205@126.com>
2017-05-02 11:48:15 +0800

    20170502

---
+++ example77.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+__author__ = "yang.dd"
+
+"""
+    example 076
+"""
+
+if __name__ == '__main__':
+    s = ["man", "woman", "girl", "boy", "sister"]
+    for i in range(len(s)):
+        print(s[i])
