07321529e480b6b2c661f2c93489b48748802597
yangdd <yangdd1205@126.com>
2017-05-07 14:38:17 +0800

    20170507

---
+++ example82.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+__author__ = "yang.dd"
+
+"""
+    example 082
+"""
+
+if __name__ == "__main__":
+    octNumber = input("Please input a octal number: ")
+    num = int(octNumber, 8)
+    print(num)
