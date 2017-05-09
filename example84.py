fb116f780b9e93ce9ad4dd7991ce49a9267a5bda
yangdd <yangdd1205@126.com>
2017-05-09 10:24:13 +0800

    20170509

---
+++ example84.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+__author__ = "yang.dd"
+
+"""
+    example 084
+"""
+
+if __name__ == "__main__":
+    delimiter = ','
+    mylist = ['Brazil', 'Russia', 'India', 'China']
+    print(delimiter.join(mylist))
